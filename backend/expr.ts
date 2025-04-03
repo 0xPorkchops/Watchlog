ab2d444ce5b6eaf972c3e5a5844a1a7b76de59d9
Sam <104501184+ByteOfMySoup@users.noreply.github.com>
2025-04-03 15:54:30 -0400

    Fixed Expr.js
    Expr.js converted to ts, and express ts definitions updated.

--- backend/expr.ts
+++ backend/expr.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
 
 app.listen(PORT, () => {
   console.log(`Express server is running on http://localhost:${PORT}`);
