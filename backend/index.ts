615bd21dfdeb1aa5f133c22e9195d9d4cca32f9e
Duyentn4 <duyentran2174@gmail.com>
2025-04-13 16:06:24 -0400

    Set up helmet in backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import helmet from 'helmet';
+
+const app = express();
+const PORT = 3000;
+
+app.use(helmet());
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
