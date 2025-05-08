e70cdd0ccc8c862355f1ebc9837a1e20a18cce51
mantraburugu <mantraburugu@gmail.com>
2025-05-08 02:07:20 -0400

    started working on buttons and gave up will continue later

--- frontend/watchlog/app/(home)/index.tsx
+++ frontend/watchlog/app/(home)/index.tsx
@@ -36,13 +36,16 @@ export default function HomeScreen() {
         <View style={styles.header}>
           <Text style={styles.logo}>WATCH LOG</Text>
           <View style={styles.nav}>
-            <Text style={styles.navLink}>Your Lists</Text>
+            {/* <Text style={styles.navLink}>Your Lists</Text>
             <Text style={styles.navLink}>Recommendations</Text>
-            <Text style={styles.navLink}>Top Ten</Text>
             <Text style={styles.navLink}>New</Text>
             <Pressable style={styles.searchBtn}>
               <Text style={styles.searchText}>Search</Text>
-            </Pressable>
+            </Pressable> */}
+            <Pressable style={styles.tabBtns} > <Text style={styles.secondaryBtnText}>Your Lists</Text> </Pressable>
+            <button style={styles.navLink} > Recommendations </button>
+            <button style={styles.navLink} > Groups </button>
+            <button style={styles.navLink} > Search </button>
           </View>
         </View>
 
@@ -149,6 +152,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
   },
+  tabBtns: {
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    backgroundColor: 'transparent', 
+    borderRadius: '12px', 
+    border: '1px solid black', 
+    marginRight: 5,
+    fontSize: 16,
+    fontWeight: '600',
+    cursor: "pointer"
+  }, 
   searchBtn: {
     backgroundColor: '#333',
     paddingVertical: 4,
