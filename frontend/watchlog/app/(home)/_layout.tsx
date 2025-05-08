094df14f44d9780202a84013440b9911641f20e3
mantraburugu <mantraburugu@gmail.com>
2025-05-08 13:56:02 -0400

    combined the backend api stuff with a working version of ui, can add the color theme stuff later

--- frontend/watchlog/app/(home)/_layout.tsx
+++ frontend/watchlog/app/(home)/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from 'expo-router/stack'
+// import { Stack } from 'expo-router/stack'
 
-export default function Layout() {
-  return <Stack />
+// export default function Layout() {
+//   return <Stack />
+// } 
+// commenting this out so i can hide the header for the demo lol 
+import { Stack } from 'expo-router';
+import React from 'react';
+
+export default function HomeLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
